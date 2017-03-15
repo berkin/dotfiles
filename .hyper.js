b72dee7cc64bd42c3426ab28ff7e71de11804aba
berkin <berkin.cirak@comeon.com>
2017-03-15 17:21:13 +0300

    hyperlinks

--- .hyper.js
+++ .hyper.js
@@ -107,7 +107,13 @@ module.exports = {
 	//   `hyperpower`
 	//   `@company/project`
 	//   `project#1.0.1`
-	plugins: ["nord-hyper", "hyper-tabs-enhanced", "hyper-pane", "hyperlayout"],
+	plugins: [
+        "nord-hyper",
+        "hyper-tabs-enhanced",
+        "hyper-pane",
+        "hyperlayout",
+        "hyperlinks"
+    ],
 
 	// in development, you can create a directory under
 	// `~/.hyper_plugins/local/` and include it here
