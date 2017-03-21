f4da0ce4171b4e32a42b5a205e2522c9e56ac3f7
berkin <berkin.cirak@comeon.com>
2017-03-21 22:54:47 +0300

    hyper tab numbers

--- .hyper.js
+++ .hyper.js
@@ -136,10 +136,17 @@ module.exports = {
 	//   `hyperpower`
 	//   `@company/project`
 	//   `project#1.0.1`
-	plugins: ["nord-hyper", "hyper-tabs-enhanced", "hyper-pane", "hyperlayout"],
+	plugins: [
+        "nord-hyper",
+        "hyper-tabs-enhanced",
+        "hyper-pane",
+        "hyperlayout",
+        "hyperterm-tab-numbers"
+    ],
 
 	// in development, you can create a directory under
 	// `~/.hyper_plugins/local/` and include it here
 	// to load it and avoid it being `npm install`ed
-	localPlugins: []
+	localPlugins:["hyperpower"]
+
 };
